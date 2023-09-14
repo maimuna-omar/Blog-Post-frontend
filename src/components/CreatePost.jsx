388915d67bf718952775ea1b5451bb7d867a0b23
maimuna omar <maymunaabdi07@gmail.com>
2023-09-14 10:23:10 +0300

    worked on css

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -34,41 +34,41 @@ function CreatePost() {
     };
 
     return (
-        <div className="m-4 md:m-8 lg:m-16 p-6 rounded-md bg-white shadow-md max-w-2xl mx-auto">
-            <h2 className="text-2xl mb-6 font-bold text-center border-b pb-3">Create a Post</h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <label htmlFor="title" className="block mb-2 font-bold">TITLE</label>
-                    <input 
-                        type="text" 
-                        id="title"
-                        name="title" 
-                        value={post.title} 
-                        onChange={handleChange} 
-                        placeholder="Enter the title"
-                        className="w-full p-2 border rounded-md"
-                    />
-                </div>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4"> {/* Adjusted these classes */}
+            <div className="rounded-md bg-white shadow-lg p-6 w-full md:max-w-2xl">
+                <h2 className="text-2xl mb-6 font-bold text-center border-b pb-3">Create a Post</h2>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label htmlFor="title" className="block mb-2 font-bold">TITLE</label>
+                        <input 
+                            type="text" 
+                            id="title"
+                            name="title" 
+                            value={post.title} 
+                            onChange={handleChange} 
+                            placeholder="Enter the title"
+                            className="w-full p-2 border rounded-md"
+                        />
+                    </div>
 
-                <div>
-                    <label htmlFor="content" className="block mb-2 font-bold">Content</label>
-                    <textarea 
-                        id="content"
-                        name="content" 
-                        value={post.content} 
-                        onChange={handleChange} 
-                        placeholder="Write your post here..."
-                        rows="5"
-                        className="w-full p-2 border rounded-md"
-                    />
-                </div>
-                
-                <button type="submit" className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">PUBLISH</button>
-            </form>
+                    <div>
+                        <label htmlFor="content" className="block mb-2 font-bold">Content</label>
+                        <textarea 
+                            id="content"
+                            name="content" 
+                            value={post.content} 
+                            onChange={handleChange} 
+                            placeholder="Write your post here..."
+                            rows="5"
+                            className="w-full p-2 border rounded-md"
+                        />
+                    </div>
+                    
+                    <button type="submit" className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">PUBLISH</button>
+                </form>
+            </div>
         </div>
     );
 }
 
 export default CreatePost;
-
-
