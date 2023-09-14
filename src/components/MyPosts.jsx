f7d2c77710fd57e0701c4e7b0e27f8fa8062ee81
maimuna omar <maymunaabdi07@gmail.com>
2023-09-14 10:30:12 +0300

    worked on css

--- src/components/MyPosts.jsx
+++ src/components/MyPosts.jsx
@@ -27,19 +27,19 @@ function MyPosts() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100 p-6">
-      <h2 className="text-black text-5xl font-bold mb-6">MY POSTS</h2>
-      <div className="grid grid-cols-2 gap-4 w-full max-w-screen-xl">
+    <div className="bg-gray-100 p-6">
+      <h2 className=" text-black text-5xl font-bold mb-6">MY POSTS</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {posts.map(post => (
-          <div key={post.id} className="bg-white border border-gray-800 rounded-lg overflow-hidden shadow-md mx-4">
+          <div key={post.id} className="bg-white border border-gray-800 rounded-lg overflow-hidden">
             <div className="p-4">
               <h4 className="text-4xl font-semibold mb-2">{post.title}</h4>
-              <p className="text-2xl mb-4">{post.content}</p> {/* Fixed the incomplete <p> tag */}
+              <p className="text-2xl mb-4">{post.content}</p>
             </div>
             <div className="flex justify-end p-4">
               <button
                 onClick={() => navigate(`/dashboard/edit-post/${post.id}`)}
-                className="bg-blue-500 hover:bg-blue-700 text-white text-4xl font-bold py-2 px-4 rounded-full"
+                className="bg-blue-500 hover:bg-blue-100 text-white text-4xl font-bold py-2 px-4 rounded-full"
               >
                 Edit Post
               </button>
